77f0946940065888fb3f69e8530aa97e7fe89f7c
huy the <thehuy168@hotmail.com>
2021-06-07 20:57:48 +0700

    Add project files.

---
+++ monitor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+
+namespace TurnScreenOFF
+{
+    class Program
+    {
+        private static int WM_SYSCOMMAND = 0x0112;
+        private static uint SC_MONITORPOWER = 0xF170;
+
+  
+        public static void Main(string[] args)
+        {
+            SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, (IntPtr)SC_MONITORPOWER, (int)2);
+            //Thread.Sleep(1000);
+            //SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, (IntPtr)SC_MONITORPOWER, (int) -1);
+
+
+
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, int lParam);
+    }
+}
